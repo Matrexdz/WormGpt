b5c97a4d5d6554ef0deae5412ab38a7e7879dc92
Matrex DZ <161771152+Matrexdz@users.noreply.github.com>
2024-05-01 02:48:09 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,30 @@
+try:
+    import requests
+    import LoopAi
+except ModuleNotFoundError:
+    import os
+    os.system("pip install requests")
+    os.system("pip install LoopAi==1.0.3")
+print(f"""\033[0m
+
+ __      __                        _____________________________
+/  \    /  \___________  _____    /  _____/\______   \__    ___/
+\   \/\/   /  _ \_  __ \/     \  /   \  ___ |     ___/ |    |   
+ \        (  <_> )  | \/  Y Y  \ \    \_\  \|    |     |    |   
+  \__/\  / \____/|__|  |__|_|  /  \______  /|____|     |____|   
+       \/                    \/          \/                     
+
+Welcome To Worm GPT , The Biggest Enemy of Chat Gpt. By Dark Lord telegram : https://t.me/matrexdz
+
+""")
+
+
+
+while True:
+    worm = LoopAi.Ai()
+
+    w2 = input("Enter your query >>> ")
+
+    response = worm.worm(w2)
+    print(response)
+
